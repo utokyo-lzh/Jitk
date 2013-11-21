0ffb93d54ae9523f61df9e693d5c96361029921d
Xi Wang <xi@mit.edu>
2013-11-21 01:20:24 -0500

    bpf: rename instructions & remove example

--- BPF.v
+++ BPF.v
@@ -4,27 +4,27 @@ Require Import compcert.lib.Maps.
 Require Import compcert.common.Memdata.
 
 Inductive instruction : Type :=
-  | ld_w_abs : int -> instruction
-  | ld_h_abs : int -> instruction
-  | ld_b_abs : int -> instruction
-  | ld_w_len : instruction
-  | ld_imm   : int -> instruction
-  | ld_mem   : int -> instruction
-  | ldx_w_imm: int -> instruction
-  | ldx_w_mem: int -> instruction
-  | ldx_w_len: instruction
-  | ldx_b_msh: int -> instruction
-  | st       : int -> instruction
-  | stx      : int -> instruction
-  | add_k    : int -> instruction
-  | add_x    : instruction
-  | ja       : int -> instruction
-  | jeq_k    : Byte.int -> Byte.int -> int -> instruction
-  | jeq_x    : Byte.int -> Byte.int -> instruction
-  | ret_k    : int -> instruction
-  | ret_a    : instruction
-  | tax      : instruction
-  | txa      : instruction
+  | Ild_w_abs : int -> instruction
+  | Ild_h_abs : int -> instruction
+  | Ild_b_abs : int -> instruction
+  | Ild_w_len : instruction
+  | Ild_imm   : int -> instruction
+  | Ild_mem   : int -> instruction
+  | Ildx_w_imm: int -> instruction
+  | Ildx_w_mem: int -> instruction
+  | Ildx_w_len: instruction
+  | Ildx_b_msh: int -> instruction
+  | Ist       : int -> instruction
+  | Istx      : int -> instruction
+  | Iadd_k    : int -> instruction
+  | Iadd_x    : instruction
+  | Ija       : int -> instruction
+  | Ijeq_k    : Byte.int -> Byte.int -> int -> instruction
+  | Ijeq_x    : Byte.int -> Byte.int -> instruction
+  | Iret_k    : int -> instruction
+  | Iret_a    : instruction
+  | Itax      : instruction
+  | Itxa      : instruction
   .
 
 Record state : Type := mkstate {
@@ -36,11 +36,11 @@ Record state : Type := mkstate {
 
 Definition empty_state := mkstate Int.zero Int.zero (ZMap.init Int.zero) 0.
 
-Definition program := list instruction.
+Definition code := list instruction.
 
 Definition packet := list byte.
 
-Definition instruction_at (p: program) (st: state) :=
+Definition instruction_at (p: code) (st: state) :=
   list_nth_z p st.(pc).
 
 Definition packet_length (b: packet) : Z :=
@@ -67,65 +67,54 @@ Definition packet_decode_int (pos: int) (len: Z) (b: packet) : option int :=
   then Some (Int.repr (int_of_bytes (rev_if_le s)))
   else None.
 
-Fixpoint interpret (fuel: nat) (p: program) (b: packet) (st: state) : state * int :=
+Fixpoint interpret (fuel: nat) (p: code) (b: packet) (st: state) : state * int :=
   match fuel with
   | O => (st, Int.zero)
   | S fuel' => match instruction_at p st with
-    | Some (ld_w_abs k) =>
+    | Some (Ild_w_abs k) =>
       match packet_decode_int k 4 b with
       | None => (st, Int.zero)
       | Some v => interpret fuel' p b (mkstate v st.(X) st.(mem) (st.(pc) + 1))
       end
-    | Some (ld_h_abs k) =>
+    | Some (Ild_h_abs k) =>
       match packet_decode_int k 2 b with
       | None => (st, Int.zero)
       | Some v => interpret fuel' p b (mkstate v st.(X) st.(mem) (st.(pc) + 1))
       end
-    | Some (ld_b_abs k) =>
+    | Some (Ild_b_abs k) =>
       match packet_decode_int k 1 b with
       | None => (st, Int.zero)
       | Some v => interpret fuel' p b (mkstate v st.(X) st.(mem) (st.(pc) + 1))
       end
-    | Some (ld_w_len) =>
+    | Some (Ild_w_len) =>
       interpret fuel' p b (mkstate (Int.repr (packet_length b)) st.(X) st.(mem) (st.(pc) + 1))
-    | Some (ld_imm k) =>
+    | Some (Ild_imm k) =>
       interpret fuel' p b (mkstate k st.(X) st.(mem) (st.(pc) + 1))
-    | Some (ld_mem k) =>
+    | Some (Ild_mem k) =>
       interpret fuel' p b (mkstate (ZMap.get (Int.unsigned k) st.(mem)) st.(X) st.(mem) (st.(pc) + 1))
-    | Some (st k) =>
+    | Some (Ist k) =>
       interpret fuel' p b (mkstate st.(A) st.(X) (ZMap.set (Int.unsigned k) st.(A) st.(mem)) (st.(pc) + 1))
-    | Some (stx k) =>
+    | Some (Istx k) =>
       interpret fuel' p b (mkstate st.(A) st.(X) (ZMap.set (Int.unsigned k) st.(X) st.(mem)) (st.(pc) + 1))
-    | Some (add_k k) =>
+    | Some (Iadd_k k) =>
       interpret fuel' p b (mkstate (Int.add st.(A) k) st.(X) st.(mem) (st.(pc) + 1))
-    | Some (add_x) =>
+    | Some (Iadd_x) =>
       interpret fuel' p b (mkstate (Int.add st.(A) st.(X)) st.(X) st.(mem) (st.(pc) + 1))
-    | Some (ja k) =>
+    | Some (Ija k) =>
       interpret fuel' p b (mkstate st.(A) st.(X) st.(mem) (st.(pc) + 1 + (Int.unsigned k)))
-    | Some (jeq_k jt jf k) =>
+    | Some (Ijeq_k jt jf k) =>
       interpret fuel' p b (mkstate st.(A) st.(X) st.(mem) (st.(pc) + 1 + (Byte.unsigned (if (Int.eq st.(A) k) then jt else jf))))
-    | Some (jeq_x jt jf) =>
+    | Some (Ijeq_x jt jf) =>
       interpret fuel' p b (mkstate st.(A) st.(X) st.(mem) (st.(pc) + 1 + (Byte.unsigned (if (Int.eq st.(A) st.(X)) then jt else jf))))
-    | Some (ret_k k) => (st, k)
-    | Some (ret_a) => (st, st.(A))
-    | Some (tax) =>
+    | Some (Iret_k k) => (st, k)
+    | Some (Iret_a) => (st, st.(A))
+    | Some (Itax) =>
       interpret fuel' p b (mkstate st.(A) st.(A) st.(mem) (st.(pc) + 1))
-    | Some (txa) =>
+    | Some (Itxa) =>
       interpret fuel' p b (mkstate st.(X) st.(X) st.(mem) (st.(pc) + 1))
     | _ => (st, Int.zero)
     end
   end.
 
-Definition interpret_main (p: program) (b: packet) :=
+Definition interpret_main (p: code) (b: packet) :=
   snd (interpret (length p) p b empty_state).
-
-(*     ldh [12]
-       jeq #ETHERTYPE_IP, L1, L2
-   L1: ret #TRUE
-   L2: ret #0
- *)
-Definition prog1 := 
-  (ld_h_abs (Int.repr 12))::        
-  (jeq_k Byte.zero Byte.one (Int.repr 2048))::
-  (ret_k Int.mone)::
-  (ret_k Int.zero)::nil.
--- test.ml
+++ test.ml
@@ -1,10 +1,28 @@
 open Camlcoq
 open BPF
 
+let byte_of n =
+  Integers.Byte.repr (Camlcoq.Z.of_uint n);;
+
+let int_of n =
+  Integers.Int.repr (Camlcoq.Z.of_uint n);;
+
+(*     ldh [12]
+       jeq #ETHERTYPE_IP, L1, L2
+   L1: ret #TRUE
+   L2: ret #0
+ *)
+let prog1 = [
+  (BPF.Ild_h_abs (int_of 12));
+  (BPF.Ijeq_k (Integers.Byte.zero, Integers.Byte.one, (int_of 2048)));
+  (BPF.Iret_k Integers.Int.mone);
+  (BPF.Iret_k Integers.Int.zero)
+];;
+
 let p1 = [];;
-let r1 = BPF.interpret_main BPF.prog1 p1;;
+let r1 = BPF.interpret_main prog1 p1;;
 print_endline (Camlcoq.Z.to_string r1);;
 
-let p2 = List.map Integers.Byte.repr (List.map Camlcoq.Z.of_uint [0;0;0;0;0;0;0;0;0;0;0;0;8;0]);;
-let r2 = BPF.interpret_main BPF.prog1 p2;;
+let p2 = List.map byte_of [0;0;0;0;0;0;0;0;0;0;0;0;8;0];;
+let r2 = BPF.interpret_main prog1 p2;;
 print_endline (Camlcoq.Z.to_string r2);;
