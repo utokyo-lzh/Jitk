9e0b31111f3eb1deed576d742258c75f4dddfadf
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-01-16 22:13:25 -0500

    checkpoint partial proof for putget

--- putgetproof.v
+++ putgetproof.v
@@ -4,6 +4,7 @@ Require Import compcert.cfrontend.Clight.
 Require Import compcert.common.Events.
 Require Import compcert.common.Smallstep.
 Require Import compcert.common.Values.
+Require Import compcert.lib.Coqlib.
 
 Definition putdef := Internal f_put.
 Definition getdef := Internal f_get.
@@ -27,6 +28,33 @@ Section s.
     Smallstep.star step2 ge putcall E0 putret /\
     Smallstep.star step2 ge getcall E0 getret ->
     vput = vget.
+  Proof.
+    destruct 1.
+
+    (* expand out all of the steps *)
+    repeat match goal with
+      | [ H: step2 ge _ _ _ |- _ ] => inv H
+      | [ H: star step2 _ _ _ _ |- _ ] => inv H
+      | [ H: function_entry2 _ _ _ _ _ _ |- _ ] => inv H
+    end; crush.
+
+    (* expand out expression evaluation *)
+    repeat match goal with
+      | [ H: Cop.sem_cast _ _ _ = Some _ |- _ ] => unfold Cop.sem_cast in H
+      | [ H: alloc_variables _ _ _ _ _ |- _ ] => inv H
+      | [ H: eval_expr _ _ _ _ _ _ |- _ ] => inv H
+      | [ H: assign_loc _ _ _ _ _ _ |- _ ] => inv H
+      | [ H: eval_lvalue _ _ _ _ _ _ _ |- _ ] => inv H; crush
+        (* crush needed to kill some dead-end goals *)
+      | [ H: deref_loc _ _ _ _ _ |- _ ] => inv H; crush
+        (* crush needed to kill some dead-end goals *)
+    end; crush.
+
+    (* just some cleanup *)
+    repeat match goal with
+      | [ H: True |- _ ] => clear H
+      | [ H: ?x = ?x |- _ ] => clear H
+    end; crush.
 
 (*
 End s.
