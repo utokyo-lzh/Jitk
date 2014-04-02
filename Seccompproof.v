9391e21b913fe27734b6ff0d4af1d7b41ca25cf7
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-04-01 23:52:54 -0400

    prove one leftover lemma, two to go

--- Seccompproof.v
+++ Seccompproof.v
@@ -187,8 +187,12 @@ Lemma psucc_ne:
   forall a b,
   a <> b -> P_of_succ_nat a <> P_of_succ_nat b.
 Proof.
-  (* XXX *)
-Admitted.
+  unfold not.
+  intros.
+  apply H.
+  apply SuccNat2Pos.inj.
+  auto.
+Qed.
 
 Lemma transl_code_label_prefix:
   forall b c prefix x c' k,
