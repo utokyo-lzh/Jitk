0bf39a3d4633def9e89ce4633ffe1f92b03d5c38
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-04-10 09:26:54 -0400

    prove second Seccompenc lemma

--- Seccompenc.v
+++ Seccompenc.v
@@ -81,12 +81,82 @@ Definition decode_bytes (l: list byte) : res encoding :=
   | _ => Error (msg "missing 8 bytes")
   end.
 
+Lemma length_1:
+  forall A:Type,
+  forall l:list A,
+  length l = 1%nat ->
+  exists a:A, l = a :: nil.
+Proof.
+  intros.
+  destruct l; crush.
+  destruct l; crush.
+  exists a; auto.
+Qed.
+
+Lemma length_2:
+  forall A:Type,
+  forall l:list A,
+  length l = 2%nat ->
+  exists a b:A, l = a :: b :: nil.
+Proof.
+  intros.
+  destruct l; crush.
+  destruct l; crush.
+  destruct l; crush.
+  exists a; exists a0; auto.
+Qed.
+
+Lemma length_4:
+  forall A:Type,
+  forall l:list A,
+  length l = 4%nat ->
+  exists a b c d:A, l = a :: b :: c :: d :: nil.
+Proof.
+  intros.
+  destruct l; crush.
+  destruct l; crush.
+  destruct l; crush.
+  destruct l; crush.
+  destruct l; crush.
+  exists a; exists a0; exists a1; exists a2; auto.
+Qed.
+
+(* From http://compcert.inria.fr/doc/html/Memdata.html *)
+Lemma decode_encode_int_1':
+  forall x, Byte.repr (decode_int (encode_int 1 (Byte.unsigned x))) = x.
+Proof.
+  intros. rewrite decode_encode_int. transitivity (Byte.repr (Byte.unsigned x)).
+  decEq. apply Zmod_small. apply Byte.unsigned_range. apply Byte.repr_unsigned.
+Qed.
+
+Lemma decode_encode_int_2':
+  forall x, Short.repr (decode_int (encode_int 2 (Short.unsigned x))) = x.
+Proof.
+  intros. rewrite decode_encode_int. transitivity (Short.repr (Short.unsigned x)).
+  decEq. apply Zmod_small. apply Short.unsigned_range. apply Short.repr_unsigned.
+Qed.
+
 Lemma encode_decode_bytes:
   forall e l,
   encode_bytes e = OK l -> decode_bytes l = OK e.
 Proof.
-  (* XXX *)
-Admitted.
+  destruct e.
+  intros.
+  monadInv H.
+  destruct (length_2 byte (encode_int 2 (Short.unsigned enc_code0)) (encode_int_length _ _)).
+  destruct (length_1 byte (encode_int 1 (Byte.unsigned enc_jt0)) (encode_int_length _ _)).
+  destruct (length_1 byte (encode_int 1 (Byte.unsigned enc_jf0)) (encode_int_length _ _)).
+  destruct (length_4 byte (encode_int 4 (Int.unsigned enc_k0)) (encode_int_length _ _)).
+  destruct H.
+  destruct H2; destruct H2; destruct H2.
+  rewrite H; rewrite H0; rewrite H1; rewrite H2; simpl.
+  rewrite <- H; rewrite <- H0; rewrite <- H1; rewrite <- H2.
+  rewrite decode_encode_int_2'.
+  rewrite decode_encode_int_1'.
+  rewrite decode_encode_int_1'.
+  rewrite decode_encode_int_4.
+  auto.
+Qed.
 
 Fixpoint seccomp_encode (c: Seccomp.code) : res (list byte) :=
   match c with
