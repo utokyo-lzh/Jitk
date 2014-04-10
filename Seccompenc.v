946586420cb235c9ac6724e1b533ac538e1e06b2
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-04-10 08:53:58 -0400

    Seccompenc: switch encoding.enc_code to short again
    This means that every encoding can be encoded into bytes and back.
    Otherwise, Z might exceed Short.modulus, and we won't be able to
    prove lemma encode_decode_bytes.

--- Seccompenc.v
+++ Seccompenc.v
@@ -20,7 +20,7 @@ Module Short := Make(Wordsize_16).
 Notation short := Short.int.
 
 Record encoding : Type := mkenc {
-  enc_code: Z;
+  enc_code: short;
   enc_jt: byte;
   enc_jf: byte;
   enc_k: int
@@ -32,22 +32,25 @@ Notation BPF_S_ALU_ADD_K := 3.
 Notation BPF_S_ALU_ADD_X := 4.
 Notation BPF_S_JMP_JA    := 41.
 
+Definition mkencx (c:Z) (jt jf:byte) (k:int) :=
+  mkenc (Short.repr c) jt jf k.
+
 Definition encode_inst (i: Seccomp.instruction) : res encoding :=
   match i with
   | Sret_a =>
-    OK (mkenc BPF_S_RET_A Byte.zero Byte.zero Int.zero)
+    OK (mkencx BPF_S_RET_A Byte.zero Byte.zero Int.zero)
   | Salu (Aaddimm k) =>
-    OK (mkenc BPF_S_ALU_ADD_K Byte.zero Byte.zero k)
+    OK (mkencx BPF_S_ALU_ADD_K Byte.zero Byte.zero k)
   | Salu Aadd =>
-    OK (mkenc BPF_S_ALU_ADD_X Byte.zero Byte.zero Int.zero)
+    OK (mkencx BPF_S_ALU_ADD_X Byte.zero Byte.zero Int.zero)
   | Sjmp_ja k =>
-    OK (mkenc BPF_S_JMP_JA Byte.zero Byte.zero k)
+    OK (mkencx BPF_S_JMP_JA Byte.zero Byte.zero k)
   | _ => Error (msg "unknown Seccomp.instruction")
   end.
 
 Definition decode_inst (e: encoding) : res Seccomp.instruction :=
   let k := e.(enc_k) in
-  match e.(enc_code) with
+  match Short.unsigned e.(enc_code) with
   | BPF_S_RET_A     => OK (Sret_a)
   | BPF_S_ALU_ADD_K => OK (Salu (Aaddimm k))
   | BPF_S_ALU_ADD_X => OK (Salu Aadd)
@@ -59,12 +62,11 @@ Lemma encode_decode_inst:
   forall i e,
   encode_inst i = OK e -> decode_inst e = OK i.
 Proof.
-  induction i; crush.
-  induction a; crush.
+  induction i; crush; try destruct a; crush.
 Qed.
 
 Definition encode_bytes (e: encoding) : res (list byte) :=
-  OK ((encode_int 2 (enc_code e)) ++
+  OK ((encode_int 2 (Short.unsigned (enc_code e))) ++
       (encode_int 1 (Byte.unsigned  (enc_jt   e))) ++
       (encode_int 1 (Byte.unsigned  (enc_jf   e))) ++
       (encode_int 4 (Int.unsigned   (enc_k    e)))).
@@ -72,10 +74,10 @@ Definition encode_bytes (e: encoding) : res (list byte) :=
 Definition decode_bytes (l: list byte) : res encoding :=
   match l with
   | [ a; b; c; d; e; f; g; h ] =>
-    OK (mkenc (decode_int [ a; b ])
-              (Byte.repr (decode_int [ c ]))
-              (Byte.repr (decode_int [ d ]))
-              (Int.repr (decode_int [ e; f; g; h ])))
+    OK (mkenc (Short.repr (decode_int [ a; b ]))
+              (Byte.repr  (decode_int [ c ]))
+              (Byte.repr  (decode_int [ d ]))
+              (Int.repr   (decode_int [ e; f; g; h ])))
   | _ => Error (msg "missing 8 bytes")
   end.
 
