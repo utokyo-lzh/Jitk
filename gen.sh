17e71d5ad52f91eef44b82429006989f82a41d2c
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-04-16 10:32:16 -0400

    fix up gen.sh
    force assembler into 32-bit mode
    
    disable assembler-level optimizations (who knew they existed?)

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 objfile=$(mktemp)
 
-./test_gen.native "$1" | gcc -x assembler - -c -o $objfile
+./test_gen.native "$1" | as --32 -n -o $objfile -
 objcopy -O binary -j .text $objfile /proc/self/fd/1
 
 rm -f $objfile
