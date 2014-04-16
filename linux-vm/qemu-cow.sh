1f1e6f30d7998bc6eb8e4700f24f3f25d1961ba8
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-04-15 20:31:35 -0400

    start on setting up a linux vm for testing stuff

---
+++ linux-vm/qemu-cow.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+qemu-img create -f qcow2 -b fs.img temp.img
+trap "rm -f temp.img" 0 1 2 3 14 15
+
+qemu-system-x86_64 \
+    -hda ./temp.img \
+    -kernel bzImage \
+    -append "root=/dev/sda init=/init.sh" \
+    -m 128 \
+    -redir tcp:9922::22 \
+    -serial telnet:localhost:12345,server,nowait
+
