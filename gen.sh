4baa684dd563b292cc8f5279cbeaeaec9b904756
Nickolai Zeldovich <nickolai@csail.mit.edu>
2014-04-16 00:31:44 -0400

    minor tweak so gen.sh runs in the qemu vm

--- gen.sh
+++ gen.sh
@@ -2,6 +2,6 @@
 objfile=$(mktemp)
 
 ./test_gen.native "$1" | gcc -x assembler - -c -o $objfile
-objcopy -O binary -j .text $objfile /dev/fd/1
+objcopy -O binary -j .text $objfile /proc/self/fd/1
 
 rm -f $objfile
