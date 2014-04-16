7e96b967d84ba20091a57d2349fa3f999a1c0d6a
David Lazar <lazard@mit.edu>
2014-04-16 14:37:22 -0400

    Add semantics for INET_DIAG.

--- inet_diag/InetDiag.v
+++ inet_diag/InetDiag.v
@@ -37,6 +37,8 @@ Inductive instruction : Type :=
 
 Definition program := list instruction.
 
+(*
+(* this probably needs something like GeneralRec in CPDT *)
 Fixpoint eval (pgm: program) (e: entry) {struct pgm} : bool :=
   match pgm with
   | nil => true
@@ -52,5 +54,43 @@ Fixpoint eval (pgm: program) (e: entry) {struct pgm} : bool :=
 with jmp (loc: location) (p: program) (e: entry) {struct p} : bool :=
   match loc with
   | Reject => false
-  | Loc n => eval p e
+  | Loc n => eval (skipn n p) e
   end.
+*)
+
+Definition checkhc (hc: hostcond) (e: entry) := false. (* XXX TODO *)
+
+Inductive state : Type :=
+  | State : program -> entry -> state
+  | RejectState : state
+  | AcceptState : state
+  .
+
+Inductive step : state -> state -> Prop :=
+  | ST_Accept : forall e,
+    step (State nil e) AcceptState
+  | ST_Nop : forall r e,
+    step (State (Nop :: r) e) (State r e)
+  | ST_Jmp_Reject : forall r e,
+    step (State (Jmp Reject :: r) e) RejectState
+  | ST_Jmp_Loc : forall r e n,
+    step (State (Jmp (Loc n) :: r) e) (State (skipn n r) e)
+  | ST_Sge : forall r e p n,
+    let r' := if Int.cmpu Cge e.(sport) p then r else Jmp n :: r in
+    step (State (Sge p n :: r) e) (State r' e)
+  | ST_Sle : forall r e p n,
+    let r' := if Int.cmpu Cle e.(sport) p then r else Jmp n :: r in
+    step (State (Sle p n :: r) e) (State r' e)
+  | ST_Dge : forall r e p n,
+    let r' := if Int.cmpu Cge e.(dport) p then r else Jmp n :: r in
+    step (State (Dge p n :: r) e) (State r' e)
+  | ST_Dle : forall r e p n,
+    let r' := if Int.cmpu Cle e.(dport) p then r else Jmp n :: r in
+    step (State (Dle p n :: r) e) (State r' e)
+  | ST_Scond : forall r e hc n,
+    let r' := if checkhc hc e then r else Jmp n :: r in
+    step (State (Scond hc n :: r) e) (State r' e)
+  | ST_Dcond : forall r e hc n,
+    let r' := if checkhc hc e then r else Jmp n :: r in
+    step (State (Dcond hc n :: r) e) (State r' e)
+  .
