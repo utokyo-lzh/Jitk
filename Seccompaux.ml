b74dc3f2f700641baa83f0fd8eee6d00f9549835
Xi Wang <xi@mit.edu>
2014-04-28 15:04:27 -0400

    change seccomp_memwords to 16
    ... since both Linux and FreeBSD use 16.

--- Seccompaux.ml
+++ Seccompaux.ml
@@ -1,7 +1,7 @@
 open BinPos
 open Camlcoq
 
-let seccomp_memwords = Camlcoq.Z.of_uint 64
+let seccomp_memwords = Camlcoq.Z.of_uint 16
 
 let sizeof_seccomp_data = coqint_of_camlint (Int32.of_int 64)
 
