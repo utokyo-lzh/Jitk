4a71323f1347bcbf823858f4eb5f0437ec185e51
Xi Wang <xi@mit.edu>
2014-06-25 17:53:56 -0400

    simplify list_length_nat_z

--- MiscLemmas.v
+++ MiscLemmas.v
@@ -10,12 +10,9 @@ Lemma list_length_nat_z:
   forall l:list A,
   list_length_z l = Z.of_nat (length l).
 Proof.
-  induction l.
-  - crush.
-  - rewrite list_length_z_cons; rewrite IHl.
-    unfold length.
-    rewrite Nat2Z.inj_succ.
-    crush.
+  induction l; crush.
+  rewrite list_length_z_cons.
+  xomega.
 Qed.
 
 Lemma length_skipn:
